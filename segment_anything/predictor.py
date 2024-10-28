92871d51daa18f5ce20961ec66a68eba139d6e40
paspf <pascal.pfeiffer@zeiss.com>
2024-10-28 15:58:15 +0100

    Use PyTorchs.to() function for Tensor dtype conversion.

--- segment_anything/predictor.py
+++ segment_anything/predictor.py
@@ -161,8 +161,8 @@ class SamPredictor:
         )
 
         masks_np = masks[0].detach().cpu().numpy()
-        iou_predictions_np = iou_predictions[0].detach().cpu().numpy()
-        low_res_masks_np = low_res_masks[0].detach().cpu().numpy()
+        iou_predictions_np = iou_predictions[0].detach().cpu().to(torch.float32).numpy()
+        low_res_masks_np = low_res_masks[0].detach().cpu().to(torch.float32).numpy()
         return masks_np, iou_predictions_np, low_res_masks_np
 
     @torch.no_grad()
